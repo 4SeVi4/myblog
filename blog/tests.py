26dda87b37791a8b017d1399a98d229ab394e6c6
Serhiy <skadnukserhiy@gmail.com>
2025-04-29 21:39:27 +0300

    Практична№5: додано шаблони, відображення та маршрути

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Article
 
-# Create your tests here.
+class ArticleModelTest(TestCase):
+    def setUp(self):
+        self.article = Article.objects.create(
+            title="Test Article",
+            content="Test content for article.",
+        )
+
+    def test_article_str(self):
+        self.assertEqual(str(self.article), "Test Article")
+
+    def test_home_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Article")
